bee14c1d03e77b8051f74364ba5b14eb94d9da43
Scott Cutler <scottharrisoncutler@gmail.com>
2017-09-06 14:10:56 -0700

    Wrote script to cause turtle to write an S (if done first thing after startup)

---
+++ scripts/turtleletter.sh
@@ -0,0 +1,3 @@
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0, 0.0, 0.0]' '[0.0, 0.0, -1.0]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[3.0, 0.0, 0.0]' '[0.0, 0.0, 4.0]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[3.0, 0.0, 0.0]' '[0.0, 0.0, -4.0]'
